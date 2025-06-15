8cbfd8898d784ef63ec7c87394218b2960596c94
denisdyukin <d.diukin@centr-to.ru>
2025-06-15 15:28:20 +0400

    feat: test app

---
+++ apps/vue-form-examples/src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { FieldProvider, FormProvider, useFormState } from '@repo/forms'
+
+const form = useFormState()
+form.setInitialState({
+  name: 'test',
+  email: 'email',
+  password: 'Gig'
+})
+
+form.onSubmit((values) => {
+  console.log('values', values)
+})
+</script>
+
+<template>
+  <main>
+    <FormProvider>
+      <FieldProvider
+        v-slot="{ ctx }"
+        name="name"
+      >
+        <input
+          v-model="ctx.value.value"
+          type="text"
+        />
+      </FieldProvider>
+      <FieldProvider
+        v-slot="{ ctx }"
+        name="email"
+      >
+        <input
+          v-model="ctx.value.value"
+          type="text"
+        />
+      </FieldProvider>
+      <FieldProvider
+        v-slot="{ ctx }"
+        name="password"
+      >
+        <input
+          v-model="ctx.value.value"
+          type="text"
+        />
+      </FieldProvider>
+    </FormProvider>
+
+    <div>
+      <button
+        type="submit"
+        @click="form.submit"
+      >
+        Submit
+      </button>
+    </div>
+    <div>
+      <pre>{{ form.currentState }}</pre>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
